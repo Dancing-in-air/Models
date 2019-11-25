a7603fa10c98deaac64c7ffe346d51b159144188
Dancing-in-air <513029b001g.cdb@sina.cn>
2019-11-25 09:21:27 +0800

    201911250921

---
+++ TaoBao.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from urllib.parse import quote
+from pyquery import PyQuery as pq
+
+browser = webdriver.Chrome()
+waite = WebDriverWait(browser, 10)  # 设置延时时间
+
+
+def index_page(page, key_word):
+    print(f"正在抓取第{page}页")
+    try:
+        url = "https://s.taobao.com/search?q" + quote(key_word)  # 对key_word进行编码
+        browser.get(url)
+        if page > 1:
+            # 延时定位输入框,在设定时间范围内返回结果,若没有结果抛出异常
+            input = waite.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#mainsrp-pager div.form > input")))
+            # 延时定位确认框,在设定时间范围内返回结果,若没有结果抛出异常
+            submit = waite.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "#mainsrp-pager div.form > span.btn J_Submit")))
+            input.clear()  # 清空输入框内容
+            input.send_keys(page)  # 键入页码
+            submit.click()  # 点击确定按钮
+        waite.until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#mainsrp-page li.item.active > span"), str(page)))
+        waite.until(EC.presence_of_element_located((By.CSS_SELECTOR, ".m-itemlist .items .item")))
+        get_products()  # 调用函数get_products,获取产品信息
+    except TimeoutException:  # 超时异常处理
+        index_page(page, key_word)
+
+
+def get_products():
+    html = browser.page_source
+    doc = pq(html)
+    items = doc("#mainsrp-itemlist .items .item").items()
+    for item in items:
+        product = {
+            "image": item.find(".pic .img").attr("data-src"),
+            "price": item.find(".price").text(),
+            "title": item.find(".title").text(),
+            "shop": item.find(".shop").text(),
+            "location": item.find(".locaiont").text()
+        }
+        print(product)
+
+
+def main():
+    index_page(1, "Ipad")
+
+
+if __name__ == '__main__':
+    main()
